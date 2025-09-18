d17644063e879909f58a98a8adf480bc3f3cbd0e
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-09-18 12:59:26 +0300

    Updates

---
+++ our-partners.js
@@ -0,0 +1,34 @@
+export const partners = [
+  {
+    id: 1,
+    image: "./assets/images/home-page/our-partners-section/coco.png",
+  },
+  {
+    id: 2,
+    image: "./assets/images/home-page/our-partners-section/rolex.png",
+  },
+  {
+    id: 3,
+    image: "./assets/images/home-page/our-partners-section/brequet.png",
+  },
+  {
+    id: 4,
+    image: "./assets/images/home-page/our-partners-section/haven.png",
+  },
+  {
+    id: 5,
+    image: "./assets/images/home-page/our-partners-section/coco.png",
+  },
+  {
+    id: 6,
+    image: "./assets/images/home-page/our-partners-section/rolex.png",
+  },
+  {
+    id: 7,
+    image: "./assets/images/home-page/our-partners-section/brequet.png",
+  },
+  {
+    id: 8,
+    image: "./assets/images/home-page/our-partners-section/haven.png",
+  },
+];
